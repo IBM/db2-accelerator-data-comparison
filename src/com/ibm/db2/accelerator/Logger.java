5757a7a7c34c53bc09293884a1c34ad84a05e66d
Björn Broll <172374935+bbroll@users.noreply.github.com>
2024-10-20 16:43:34 +0200

    Code for comparing data in Db2 for z/OS and the accelerator

---
+++ src/com/ibm/db2/accelerator/Logger.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright contributors to the idaadiff project
+ */
+package com.ibm.db2.accelerator;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Minimal log infrastructure
+ */
+public class Logger {
+
+	private static Logger gLogger;
+	private boolean debugMode = false;
+	
+	public boolean isDebugMode() {
+		return debugMode;
+	}
+
+	public void setDebugMode(boolean debugMode) {
+		this.debugMode = debugMode;
+	}
+
+	public static Logger getInstance() {
+		if (gLogger == null) {
+			gLogger = new Logger();
+		}
+		return gLogger;
+	}
+
+	public void addLogEntry(String logEntry) {
+		System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) + " " + logEntry);
+	}
+	
+	public void addLogEntryDebug(String logEntry) {
+		if (debugMode) {
+			addLogEntry(logEntry);
+		}
+	}
+	
+}
